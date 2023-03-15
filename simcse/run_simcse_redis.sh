8382e67950d473d207ec009f726bcfbaf4d5017a
chengniu <chengniu@gmail.com>
2023-03-15 12:32:36 +0800

    Update run_simcse_redis.sh

--- simcse/run_simcse_redis.sh
+++ simcse/run_simcse_redis.sh
@@ -1,11 +1,12 @@
 set -e
 set -x
 
-#/home/services/chengniu/SimCSE 0 /mnt/nlp/search/relevance/simcse-gnews-bert-base-uncased
-#/home/services/chengniu/SimCSE 0 /mnt/nlp/search/relevance/simcse-gnews_title-bert-base-uncased
+#/home/services/chengniu/SimCSE 0 /mnt/nlp/search/relevance/simcse-gnews-bert-base-uncased simcse-gnew
+#/home/services/chengniu/SimCSE 0 /mnt/nlp/search/relevance/simcse-gnews_title-bert-base-uncased simcse-gnews_title
 src=$1
 gpu=$2
 model=$3
+redis_q=$4
 
 
 export PYTHONPATH=$PYTHONPATH:$src
@@ -30,3 +31,4 @@ python ./SimCseRedis.py \
     --temp 0.05 \
     --do_train true \
     --do_eval false \
+    --redis_q $redis_q \
